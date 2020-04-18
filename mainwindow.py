20435ce4350fb5417632f82d23f46e58f985d70a
bobrobotsun <bobrobotsun@163.com>
2020-04-18 08:40:53 +0800

    版本号：1.0.20200418.1 更新了7.26

--- mainwindow.py
+++ mainwindow.py
@@ -33,7 +33,7 @@ class Main(QMainWindow):
         self.initUI()
 
     def initParam(self):
-        self.version = '7.25c'
+        self.version = '7.26'
         self.title = 'dotawiki'
         # 登录用的一些东西，包括网址、request（包含cookie）、api指令
         self.target_url = 'https://dota.huijiwiki.com/w/api.php'
